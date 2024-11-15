275281bc8a89a5b2e28dca7afba44f14998ffa99
hasibur14 <hasib141415@gmail.com>
2024-11-15 23:18:48 +0600

    [ADD] blogs details

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,45 +1,40 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Blogs = () => {
 
-    return (
-        <div className="grid grid-cols-3 gap-10 my-40">
-            {
-                blogs?.map(blog => (
-                    <div key={blog.id} className="w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-                        <img className="object-cover w-full h-56" src={blog.image} alt="avatar" />
+    const [blogs, setBlogs] = useState([]);  
 
-                        <div className="py-5 text-center">
-                            <a href="#" className="block text-xl font-bold text-gray-800 dark:text-white" tabIndex="0" role="link">{blog.name}</a>
-                            <span className="text-sm text-gray-700 dark:text-gray-200">{blog.title}</span>
+    useEffect(() => {
+        fetch("/blogs.json")
+        .then(res => res.json())
+        .then( data => setBlogs(data))
+    }, [])
+
+    return (
+        <div className="container mx-auto">
+            <div className="grid grid-cols-3 gap-10 my-40">
+                {
+                    blogs?.map(blog => (
+                        <div key={blog.id} className="w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                            <img className="object-cover w-full h-56" src={blog.image} alt="avatar" />
+
+                            <div className="py-5 text-center">
+                                <a href="#" className="block text-xl font-bold text-gray-800 dark:text-white" tabIndex="0" role="link">{blog.name}</a>
+                                <span className="text-sm text-gray-700 dark:text-gray-200">{blog.title}</span>
+                            </div>
+                            <Link to={`/details-blogs/${blog.id}`}>
+                                <button className="btn  bg-green-500 text-white hover:bg-green-700">Details</button>
+                            </Link>
                         </div>
-                    </div>
-                ))
-            }
+                    ))
+                }
+            </div>
         </div>
     );
 };
 
 export default Blogs;
 
-const blogs = [
-    {
-        id: 1,
-        image: 'https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        name: "Anim",
-        title: "Developer"
-    },
-    {
-        id: 2,
-        image: 'https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        name: "Nazmul",
-        title: "Developer"
-    },
-    {
-        id: 3,
-        image: 'https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        name: "Rafi",
-        title: "Developer"
-    }
 
-]
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import About from "../Pages/About/About";
 import Blogs from "../Pages/Blogs/Blogs";
+import BlogsDetails from "../Pages/Blogs/BlogsDetails";
 import Home from "../Pages/Home/Home";
 
 
@@ -9,26 +10,34 @@ import Home from "../Pages/Home/Home";
 
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout/>,
-      children  : [
-        {
-            path: "/",
-            element : <Home/>
-        },
-        {
-            path : "/about",
-            element: <About/>
-        },
-        {
-            path: "/blogs",
-            element: <Blogs/>
-        }
-      ]
-  
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />
+      },
+      {
+        path: "/details-blogs/:id",
+        element: <BlogsDetails/>,
+        loader: ({ params }) =>
+          fetch(`/blogs.json`)
+        .then(res => res.json())
+        .then(data => data.find(blog => blog.id === Number(params.id) ) )
+      }
+    ]
+
+  },
+]);
 
 
-  export default router;
+export default router;
